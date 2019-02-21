5116c4bcb0a90659e5f040efbd1e8d836b3f184d
CookiesDeathCookies <azotx12345@gmail.com>
2019-02-21 16:57:53 +0300

    Fix typo

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
