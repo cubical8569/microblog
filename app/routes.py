332653a97ac661cb32571f7528231648a6a35d8b
CookiesDeathCookies <azotx12345@gmail.com>
2019-02-23 00:37:21 +0300

    Add additional login logic

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Michael'}
     posts = [
         {
             'author': {'username': 'Venediktov'},
@@ -23,7 +23,7 @@ def index():
         }
     ]
 
-    return render_template('index.html', title='Home', user=user, posts=posts) 
+    return render_template('index.html', title='Home', posts=posts) 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -39,7 +39,12 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+    
+        return redirect(next_page)
 
     return render_template('login.html', title='Sign in', form=form);
 
