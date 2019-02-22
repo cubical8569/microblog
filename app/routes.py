15cadb658b77fdd22f541e64756271c551bd052c
CookiesDeathCookies <azotx12345@gmail.com>
2019-02-23 01:58:58 +0300

    Finish registration

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
@@ -52,3 +52,19 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations! You\'re now a registered user.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
