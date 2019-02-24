f9db0fda3621f6d3fd1321e0a16e7fe56b33a3d2
CookiesDeathCookies <azotx12345@gmail.com>
2019-02-24 22:17:54 +0300

    Add user avatars and post subtemplate

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
 from flask_login import UserMixin
+from hashlib import md5 
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +17,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
