24880c4b96a4fb35f17ff2f6089bd77694f4ab04
CookiesDeathCookies <azotx12345@gmail.com>
2019-02-27 02:34:40 +0300

    Add email configuration

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_PORT') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    ADMINS = ['azotx12345@gmail.com']
