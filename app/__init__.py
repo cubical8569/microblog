d0808b0dfc92e55950730fd7cda4bb531d88f8f6
CookiesDeathCookies <azotx12345@gmail.com>
2019-02-27 03:21:39 +0300

    Add logging to a file

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -36,3 +37,27 @@ if not app.debug:
 
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    
+    file_handler = RotatingFileHandler(
+            'logs/microblog.log', 
+            maxBytes=10240,
+            backupCount=10
+    )
+
+    file_handler.setFormatter(
+            logging.Formatter(
+                    '%(asctime)s %(levelname)s: %(message)s '\
+                    '[in %(pathname)s:%(lineno)d]'
+                )
+            )
+
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
